9ea8f6cc72a9ea39c25a1b46e479c138b97bdca3
tekkukerli <tekkukerli@gmail.com>
2019-09-30 20:39:55 +0300

    öökulli joonistus parandus3

--- muutujad/kommentaarid.php
+++ muutujad/kommentaarid.php
@@ -23,9 +23,9 @@ echo "Sinu kaal: $kaal<br>";
 echo "$nimi on $vanus aastat vana ja kaalub $kaal kg<br>";
 echo "\"It`s all right, Ma (I`m Only Bleeding)\"-Bob Dylan<br>";
 
-echo " \_____/<br>";
-echo  " (@,@)<br>";
-echo  "((| |))<br>";
+echo "\_____/<br>";
+echo " (@,@)<br>";
+echo "((| |))<br>";
 echo "__,,_,,__<br>";
 
 
