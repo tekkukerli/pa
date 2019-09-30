9aed128255fab902e36a6980474996ce3ab18370
tekkukerli <tekkukerli@gmail.com>
2019-09-30 20:35:34 +0300

    öökulli joonistus

--- muutujad/kommentaarid.php
+++ muutujad/kommentaarid.php
@@ -20,8 +20,13 @@ echo "Sinu kaal: $kaal<br>";
 
 //ülesanne 1 Kerli Tekku VSo19
 
-echo "$nimi on $vanus aastat vana ja kaalub $kaal kg.";
-echo "\"It`s all right, Ma (I`m Only Bleeding)\"-Bob Dylan";
+echo "$nimi on $vanus aastat vana ja kaalub $kaal kg<br>";
+echo "\"It`s all right, Ma (I`m Only Bleeding)\"-Bob Dylan<br>";
+
+ \_____/
+  (@,@)
+ ((| |))
+__,,_,,__
 
 
 
