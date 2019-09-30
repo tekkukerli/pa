48516b9c9305e3a6b9c292d3d78e98de2eaf3565
tekkukerli <tekkukerli@gmail.com>
2019-09-30 20:22:31 +0300

    jutumärkide kasutamise näide

--- muutujad/kommentaarid.php
+++ muutujad/kommentaarid.php
@@ -15,13 +15,13 @@ $nimi = $eNimi." ".$pereNimi;
 $kaal = 55.5;
 
 echo "Tere $nimi!<br>";
-echo "Sinu vanus:$vanus<br>";
-echo "Sinu kaal:$kaal<br>";
+echo "Sinu vanus: $vanus<br>";
+echo "Sinu kaal: $kaal<br>";
 
 //ülesanne 1 Kerli Tekku VSo19
 
 echo "$nimi on $vanus aastat vana ja kaalub $kaal kg.";
-
+echo "It`s all right, Ma (I`m Only Bleeding)"-Bob Dylan;
 
 
 
