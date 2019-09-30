104d3230050fe14e819aa97d7293b7e1f91beed0
tekkukerli <tekkukerli@gmail.com>
2019-09-30 20:55:30 +0300

    joonistuse parandus3

--- muutujad/kommentaarid.php
+++ muutujad/kommentaarid.php
@@ -27,7 +27,7 @@ echo "&nbsp;\_____/<br>";
 echo "&nbsp;&nbsp;(@,@)<br>";
 echo "&nbsp;&nbsp;&nbsp;((|&nbsp;|))<br>";
 echo "__,,_,,__<br>";
-"<br>"
+
 echo "&nbsp;\&nbsp;|&nbsp;/<br>";
 echo "@&nbsp;&nbsp;@<br>";
 echo "&nbsp;&nbsp;&nbsp;*&nbsp;&nbsp;<br>";
