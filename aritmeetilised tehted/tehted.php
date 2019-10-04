af6ed4fb47c144b0d89816d8eca5562f276d0db5
tekkukerli <tekkukerli@gmail.com>
2019-10-04 09:41:00 +0300

    tabeli sisestamine

--- aritmeetilised tehted/tehted.php
+++ aritmeetilised tehted/tehted.php
@@ -2,11 +2,79 @@
 //aritmeetilised operaatorid
 $x = 8;
 $y = 2;
-$liitm = $x + $y;
+$liitm = $x + $y; //fikseeritakse valem
 $lahut = $x - $y;
 $korru = $x * $y;
 $jagam = $x / $y;
 $jaak = $x % $y;
-echo "Vastused: $liitm, $lahut, $korru, $jagam, $jaak";
+echo "Vastused: $liitm, $lahut, $korru, $jagam, $jaak<br>"; //vastuste kuvamine
+
+//tabeli väljastamine
+echo
+"<!doctype html>
+<html lang='en'>
+<head>
+<meta charset='UTF-8'>
+<meta name='viewport' content='width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0'>
+<meta http-equiv='X-UA-Compatible' content='ie=edge'>
+<title>Document</title>
+<style>
+    table, th, td {
+        border: 1px solid black;
+        padding: 1rem;
+        text-align: center;
+        border-collapse: collapse;
+    }
+    thead {
+        background: orange;
+    }
+</style>
+</head>
+
+
+<table>
+  <thead>
+    <tr>
+      <td>Operaator</td>
+      <td>Nimetus</td>
+      <td>Näide</td>
+      <td>Tulemus</td>
+    </tr>
+</thead>
+
+<tbody>
+  <tr>
+    <td>x + y</td>
+    <td>Liitmine</td>
+    <td>".$x." + ".$y."</td>
+    <td>".$liitm."</td>
+  </tr>
+  <tr>
+    <td>x - y</td>
+    <td>Lahutamine</td>
+    <td>".$x." - ".$y."</td>
+    <td>".$lahut."</td>
+  </tr>
+  <tr>
+    <td>x * y</td>
+    <td>Korrutamine</td>
+    <td>".$x." * ".$y."</td>
+    <td>".$korru."</td>
+  </tr>
+  <tr>
+    <td>x / y</td>
+    <td>Jagamine</td>
+    <td>".$x." / ".$y."</td>
+    <td>".$jagam."</td>
+  </tr>
+  <tr>
+    <td>x % y</td>
+    <td>Jääk</td>
+    <td>".$x." % ".$y."</td>
+    <td>".$jaak."</td>
+  </tr>
+</tbody>
+</table>
+</html>";
 
 ?>
