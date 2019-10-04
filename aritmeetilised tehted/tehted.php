571ff2b9cdf4b643599e2987bf3350575af41e41
tekkukerli <tekkukerli@gmail.com>
2019-10-04 09:08:55 +0300

    tehted

---
+++ aritmeetilised tehted/tehted.php
@@ -0,0 +1,12 @@
+<?php
+//aritmeetilised operaatorid
+$x = 8;
+$y = 2;
+$liitm = $x + $y;
+$lahut = $x - $y;
+$korru = $x * $y;
+$jagam = $x / $y;
+$jaak = $x % $y;
+echo "Vastused: $liitm, $lahut, $korru, $jagam, $jaak";
+
+?>
