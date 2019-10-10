6fa2b6a6c2ce9a514ef023babe9ff2f03efd6570
tekkukerli <tekkukerli@gmail.com>
2019-10-10 11:41:10 +0300

    tingimuslaused ülesanded

---
+++ tingimuslaused/tingimuslause.php
@@ -0,0 +1,72 @@
+<?php
+$x = 3;
+$y = 2;
+var_dump($x == $y);
+echo '<hr>';
+
+$varv = 'punane';
+if($varv){  //kontrollob kas muutujale on väärtus omistatud
+    echo 'Värv on lisatud!';
+}
+echo '<hr>';
+$varv = '';
+if(!$varv){
+    echo 'Muutujal pole väärtust!';
+}
+echo '<hr>';
+$arv = 7;
+$jaak = $arv%2;
+
+if($jaak == 0){
+    echo 'Arv on paaris';
+} else {
+    echo 'Arv on paaritu';
+}
+echo '<hr>';
+
+
+//defineeri arv
+$arv = -9;
+if($arv > 0) {
+    echo $arv. ' on positiivne';
+}   else if($arv < 0) {
+    echo $arv. ' on negatiivne';
+}   else { //kui arv ei ole > kui 0
+    echo $arv.' on 0';
+}
+echo '<hr>';
+
+//kompaktkuju
+$user = 'admin';
+echo $user == 'admin' ? 'Tere admin' : 'Tere külaline';
+echo '<hr>';
+/*
+//ÜLESANNE 4.1
+$arv1 = 10;
+$arv2 = 5;
+$tehe = $arv1 / $arv2;
+
+if ($arv2 == 0) {
+    echo 'Nulliga ei saa jagada<br>';
+  } else {
+    echo $arv1 ; //POOLELI!!!!!
+  }
+*/
+//4.2
+echo '<hr>';
+$hetkeAasta = 2019;
+$synniAasta = 2001;
+$vanus = $hetkeAasta - $synniAasta;
+
+$jaak = $vanus % 5;
+
+if($jaak == 0){
+    echo 'Sel aastal on juubel!<br>';
+} else {
+    echo 'Veel juubelit ei ole<br>';
+}
+
+
+
+
+?>
