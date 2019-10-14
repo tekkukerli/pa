9c13ec420ebbd0e54727bd7ce72f7d628be07ded
tekkukerli <tekkukerli@gmail.com>
2019-10-14 19:23:11 +0300

    muutujate fail täiendatud

--- Tsüklid/tsüklid.php
+++ Tsüklid/tsüklid.php
@@ -26,6 +26,68 @@ for ($nr = 1; $nr < 11; $nr++) {
 }
 echo '<hr>';
 
+//tärniruut
+
+for($reaNr = 1; $reaNr < 6; $reaNr++) {
+    echo $reaNr;
+//tsükkel tsüklis
+    for($veeruNr = 1; $veeruNr < 7; $veeruNr++) {
+        echo '*';
+    }
+    echo '<br>';
+}
+echo '<hr>';
+
+
+for($reaNr = 1; $reaNr < 6; $reaNr++) {
+    echo $reaNr;
+//tsükkel tsüklis
+    for($veeruNr = 1; $veeruNr <= $reaNr; $veeruNr++) {
+        echo '*';
+    }
+    echo '<br>';
+}
+echo '<hr>';
+
+//ÜLESANNE 6.1
+
+// ridade ja veergude max
+$ridadeArv = 7;
+$veergudeArv = 7;
+// ridade määramine ja vahetus
+for($reaNr = 1; $reaNr <= $ridadeArv; $reaNr++){
+    echo $reaNr;
+    // veergude määrame ja vahetus
+    for($veeruNr = 1; $veeruNr <= $reaNr; $veeruNr++){
+        if($veeruNr != $reaNr){
+            echo '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;';
+        } else {
+            echo '*';
+        }
+    }
+    echo '<br>';
+}
+echo '<hr>';
+
+//PI ARVUTAMINE
+
+$kord = 1;
+$kordMax = 1000;
+$summa = 0;
+
+while($kord <= $kordMax) {
+    $element = 1 / (2 * $kord -1);
+    $jaak = $kord % 2;
+    if($jaak == 0) {
+        $summa = $summa - $element; {
+        } else {
+          $summa = $summa + $element;
+        }
+        $kord ++;
+    }
+    $pi = $summa * 4;
+    echo $pi.'<br>';
+}
 
 
 ?>
--- muutujad/kommentaarid.php
+++ muutujad/kommentaarid.php
@@ -8,22 +8,42 @@ rida
 
 //üherealine kommentaar
 
-$eNimi = "Kerli";
+//muutujate loomine
+$nimi = "Kerli";
 $pereNimi = "Tekku";
 $vanus = 27;
-$nimi = $eNimi." ".$pereNimi;
+$kaal = 55.5;
 $sugu = "naine";
 
-echo "Tere $nimi!<br>";
+//muutujate väljastamine
+echo "Tere $nimi $pereNimi!<br>";
 echo "Sinu vanus: $vanus<br>";
 echo "Sinu sugu: $sugu<br>";
+echo "Sinu kaal: $kaal<br>";
+
+//kahekordsed jutumärgid
+echo "$nimi on $vanus aastane<br>";
+
+//ühekordsed jutumärgid
+/*
+echo '$nimi on $vanus aastane<br>';
+*/
+//muutuja käänamine
+echo "${nimi}l on kiire auto!<br>";
+
+//jutumärkide kuvamine
+echo '"Welcome to Estonia"<br>';
+echo "Shakespeare'i teosed<br>";
+echo "\"It's my life\" - Dr. Alban<br>";
 
 //ülesanne 1 Kerli Tekku VSo19
 
 echo "$nimi on $vanus aastat vana ja sugu $sugu.<br>";
+
 echo "\"It`s all right, Ma (I`m Only Bleeding)\"- Bob Dylan<br><br>";
 
 //joonistus
+echo '<hr>';
 echo "&nbsp;\_____/<br>";
 echo "&nbsp;&nbsp;(@,@)<br>";
 echo "&nbsp;&nbsp;&nbsp;((|&nbsp;|))<br>";
