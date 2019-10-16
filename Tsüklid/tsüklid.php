2690a8e7d68d0bce5dda7e65b13de39643264214
tekkukerli <tekkukerli@gmail.com>
2019-10-16 22:16:42 +0300

    tsüklid fail täiendatud

--- Tsüklid/tsüklid.php
+++ Tsüklid/tsüklid.php
@@ -1,7 +1,6 @@
 <?php
-
-
-for ($kordArv = 1; $kordArv < 10; $kordArv++) {
+// tsükkel *
+for ($kordArv = 1; $kordArv < 11; $kordArv++) {
     echo $kordArv.'. ';
     echo '*';
     echo '<br>';
@@ -11,7 +10,7 @@ echo '<hr>';
 //paarisarvude kuvamine
 for ($nr = 1; $nr < 11; $nr++) {
     $jaak = $nr % 2;
-    if ($jaak == 0){
+    if ($jaak == 0){ //jääk võrdub null, seega jagub, seega paaris arv
         echo $nr.'<br>';
     }
 }
@@ -20,17 +19,17 @@ echo '<hr>';
 //paaritute arvude kuvamine
 for ($nr = 1; $nr < 11; $nr++) {
     $jaak = $nr % 2;
-    if ($jaak != 0){
+    if ($jaak != 0){ //kui jääk ei võrdu null, ei jagu, ei ole paaris
         echo $nr.'<br>';
     }
 }
+
 echo '<hr>';
 
 //tärniruut
-
 for($reaNr = 1; $reaNr < 6; $reaNr++) {
     echo $reaNr;
-//tsükkel tsüklis
+     //tsükkel tsüklis
     for($veeruNr = 1; $veeruNr < 7; $veeruNr++) {
         echo '*';
     }
@@ -38,10 +37,10 @@ for($reaNr = 1; $reaNr < 6; $reaNr++) {
 }
 echo '<hr>';
 
-
+// kasvav *
 for($reaNr = 1; $reaNr < 6; $reaNr++) {
     echo $reaNr;
-//tsükkel tsüklis
+    //tsükkel tsüklis
     for($veeruNr = 1; $veeruNr <= $reaNr; $veeruNr++) {
         echo '*';
     }
@@ -49,7 +48,7 @@ for($reaNr = 1; $reaNr < 6; $reaNr++) {
 }
 echo '<hr>';
 
-//ÜLESANNE 6.1
+//ÜLESANNE 6.1  diagonaal
 
 // ridade ja veergude max
 $ridadeArv = 7;
@@ -57,7 +56,7 @@ $veergudeArv = 7;
 // ridade määramine ja vahetus
 for($reaNr = 1; $reaNr <= $ridadeArv; $reaNr++){
     echo $reaNr;
-    // veergude määrame ja vahetus
+    // veergude määramine ja vahetus
     for($veeruNr = 1; $veeruNr <= $reaNr; $veeruNr++){
         if($veeruNr != $reaNr){
             echo '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;';
@@ -70,15 +69,15 @@ for($reaNr = 1; $reaNr <= $ridadeArv; $reaNr++){
 echo '<hr>';
 
 //PI ARVUTAMINE
-
+/*
 $kord = 1;
 $kordMax = 1000;
 $summa = 0;
 
 while($kord <= $kordMax) {
     $element = 1 / (2 * $kord -1);
-    $jaak = $kord % 2;
-    if($jaak == 0) {
+    $jaak1 = $kord % 2;
+    if($jaak1 == 0) {
         $summa = $summa - $element; {
         } else {
           $summa = $summa + $element;
@@ -88,6 +87,28 @@ while($kord <= $kordMax) {
     $pi = $summa * 4;
     echo $pi.'<br>';
 }
+*/
+//ÜL 6.2
+$i=5; //read 5
+$j=5; //veerud 5
+
+for($i=1;$i<=5;$i++) {
+    for($j=1;$j<=$i;$j++) {
+        echo $j;
+    }
+    echo '<br />';
+}
+
+echo '<hr>';
+
+//ÜL 6.4
+
+
+
+
+
+
+
 
 
 ?>
