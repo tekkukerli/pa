1d348b01690917785a30e73cc6a04187d4626886
tekkukerli <tekkukerli@gmail.com>
2019-11-08 20:41:47 +0200

    lisasin funkts

---
+++ funktsioonid/funktsioonid.php
@@ -0,0 +1,56 @@
+<?php
+/*
+function tervita(){
+    return "Tere kylaline";
+}
+echo tervita();
+
+
+function tervita($nimi="Kylaline"){
+    return "Tere $nimi";
+}
+echo tervita("Kerli");
+
+
+function tervita($nimi="Kylaline"){
+    return "Tere $nimi";
+}
+echo tervita();
+
+
+function tervita($enimi="Hea", $pnimi="Kylaline"){
+    return "Tere $enimi $pnimi";
+}
+echo tervita();
+echo "<br>";
+echo tervita("Kerli", "Tekku");
+echo "<br>";
+echo tervita("Imre", "Tard");
+*/
+
+
+//Matemaatilised funktsioonid
+/*
+echo min(11,21,32,43,54);
+echo "<br>";
+echo max(11,21,32,43,54);
+echo "<br>";
+$arvud = array(11,21,32,43,54,60);
+echo max($arvud);
+echo "<br>";
+$arvud1 = array(11,21,32,43,54);
+$arvud2 = array(60,72,42,34,45);
+echo max(max($arvud1), max($arvud2));
+*/
+$arv = 3.456;
+
+echo round($arv);
+echo round($arv,2);
+
+$arv = 3.456;
+
+echo ceil($arv);		//4
+echo floor($arv);
+
+echo rand();
+?>
